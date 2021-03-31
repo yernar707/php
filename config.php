75a0ad741e640e02a9ca006711ed91f38faf9bff
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2021-03-31 16:21:25 +0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	$hn = "localhost";  //host where our database is
+	$u = "root";  //username to access to database
+	$p = "";  //password to access to the user
+	$db= "amir";  //name of the database
+	$connect = mysqli_connect($hn, $u, $p, $db);  //connect to the database
+?>
