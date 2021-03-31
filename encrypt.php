4f537d53bd9154e39f2126ca812d49c745514626
Yernar Yergaziyev <54137699+yernar707@users.noreply.github.com>
2021-03-31 16:47:37 +0600

    Update encrypt.php

--- encrypt.php
+++ encrypt.php
@@ -5,15 +5,15 @@
   $method = "AES-128-CTR"; //ciphering method
 
   //We will use OpenSSL encryption
-  $iv_len = openssl_cipher_iv_length($ciphering);
+  $iv_len = openssl_cipher_iv_length($method);
   $opts = 0;
 
-  $enc_iv = '1234567891011121'; //initialization vector, it mustn't be null and 16 digit, we will use it to decrypt, too
+  $enc_iv = '1234567898765432'; //initialization vector, it mustn't be null and 16 digit, we will use it to decrypt, too
 
   $enc_key = "ThisIsYourKey"; //this is our key to encrypt, we will use it to decrypt, too
 
   //the function used for OpenSSL encryption
-  $result = openssl_encrypt($simple_string, $ciphering, $encryption_key, $options, $encryption_iv);
+  $result = openssl_encrypt($str, $method, $enc_key, $opts, $enc_iv);
   
   echo "Intital string: " . $str . "\n";
   echo "Result: " . $result . "\n";
